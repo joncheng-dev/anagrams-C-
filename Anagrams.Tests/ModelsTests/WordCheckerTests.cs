901aedf20e6677df84bc3cab0fc0829b6d12526a
Jon Cheng <joncheng.dev@gmail.com>
2023-09-19 10:52:07 -0700

    set up boilerplate for project
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>

---
+++ Anagrams.Tests/ModelsTests/WordCheckerTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagrams.Models;
+
+namespace Anagrams.Tests
+{
+  [TestClass]
+  public class WordCheckerTests
+  {
+    // Test methods go here
+  }
+}
---
+++ Anagrams/Models/WordChecker.cs
@@ -0,0 +1,7 @@
+namespace Anagrams.Models
+{
+  public class WordChecker
+  {
+    // properties, constructors, methods, etc. go here
+  }
+}
